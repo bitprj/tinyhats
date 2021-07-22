ccb64e3807ea7737dba9fec839d7e6a6c4c88e8e
emsesc <emseschen@gmail.com>
2021-07-22 10:17:03 -0400

    revert and add baby

--- services/manipulation-service/src/image.js
+++ services/manipulation-service/src/image.js
@@ -1,21 +1,9 @@
 const Jimp = require('jimp')
 const Canvas = require('canvas')
-const kittydar = require('kittydar')
 
-const findCat = (cat) => {
-  var img = new Canvas.Image; // creating an image object
-  img.src = cat;
+const findBaby = (baby) => {
 
-  var w = img.width;
-  var h = img.height;
-
-  var canvas = new Canvas(w, h);
-  var ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0, w, h, 0, 0, w, h);
-
-  var cats = kittydar.detectCats(canvas);
-  console.log('There are', cats.length, 'cats in this photo');
 
 }
 
-exports.findCat = findCat
+exports.findBaby = findBaby
