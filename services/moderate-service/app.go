0a409c72d9fddf71b8cf742dd07b33f5b056937f
emsesc <emseschen@gmail.com>
2021-07-23 10:26:24 -0400

    parsing params

--- services/moderate-service/app.go
+++ services/moderate-service/app.go
@@ -12,8 +12,20 @@ func main() {
 	app := fiber.New()
 
 	// Hello, World!
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+	app.Get("/moderate", func(c *fiber.Ctx) error {
+		var approval string = c.Query("approve")
+		var id string = c.Query("id")
+		var result string
+
+		if approval == "true" {
+			result = `Thank you for approving! The image had an id of ` + id
+		} else if approval == "false" {
+			result = "We're sorry that image " + id + " wasn't suitable. Thanks for your help!"
+		} else if approval == "" || id == "" {
+			result = "Please access a link from a valid moderation email."
+		}
+
+		return c.SendString(result)
 	})
 
 	// Listen and Server in 0.0.0.0:$PORT
