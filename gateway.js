698d94de859004db65cc8ab5cfaff10cd1fbad0e
Daniel Kim <dgkim@ucdavis.edu>
2021-08-01 18:31:57 -0700

    change to :apiName --emily

--- gateway.js
+++ gateway.js
@@ -10,57 +10,65 @@ const PORT = 4444
 // for testing locally: node -r dotenv/config index.js  
 // https://stackoverflow.com/questions/28305120/differences-between-express-router-and-app-get
 
+router.post('/:apiName', upload.any(), async (req, res) => {
+    console.log(`[!] ${req.params.apiName} was accessed.`)
+    let route = req.params.apiName
+
+    if (route == "add") {
+        let image = req.files[0].buffer
+        let name = req.body.name
+        console.log("Requesting moderation and uploading image...")
+    
+        // hit the add endpoint to add image and begin approval process
+        let formData = new FormData()
+        formData.append('file', image, {filename: "baby", data: image})
+        formData.append('name', name)
+        const formHeaders = formData.getHeaders();
+    
+        console.log(process.env.ADD_ENDPOINT)
+        
+        const addResp = await fetch(`http://localhost:80/add`, {
+            method: 'POST',
+            body: formData,
+                headers: {
+                ...formHeaders,
+                },        
+        });
+    
+        var result = await addResp.json()
+        console.log(`Received from /add: ${JSON.stringify(result)}`)
+        res.send({result})
+    } else if (route == "") {
+        const addResp = await fetch(`http://${process.env.FETCH_ENDPOINT}/fetch`, {
+            method: 'GET',      
+        });
+    
+        console.log("Fetching base64 image")
+    
+        var result = await addResp.json()
+        res.send({result}) 
+    }
+})
+
+router.get('/:apiName', upload.any(), async (req, res) => {
+    console.log(`[!] ${req.params.apiName} was accessed.`)
+    res.end(req.params.apiName + '\n')
+
+    let route = req.params.apiName;
+    if (route == "moderate") {
+        let approve = req.query.approve;
+        let id = req.query.id;
+        const moderateResp = await fetch(`http://localhost:80/moderate?approve=${approve}&id=${id}`, {
+            method: 'GET'
+        })
+    
+        var result = await moderateResp.text()
+        res.send({result})
+    }
+})
 
 app.use('/', router)
 
 app.listen(PORT, () => {
     console.log(`API Gateway started on port ${PORT}`)
-})
-
-router.post('/add', upload.any(), async(req, res) => {
-    let image = req.files[0].buffer
-    let name = req.body.name
-    console.log("Requesting moderation and uploading image...")
-
-    // hit the add endpoint to add image and begin approval process
-    let formData = new FormData()
-    formData.append('file', image, {filename: "baby", data: image})
-    formData.append('name', name)
-    const formHeaders = formData.getHeaders();
-
-    console.log(process.env.ADD_ENDPOINT)
-    
-    const addResp = await fetch(`http://${process.env.ADD_ENDPOINT}/add`, {
-        method: 'POST',
-        body: formData,
-            headers: {
-            ...formHeaders,
-            },        
-    });
-
-    var result = await addResp.json()
-    console.log(`Received from /add: ${JSON.stringify(result)}`)
-    res.send({result}) 
-});
-
-router.get('/', upload.any(), async(req, res) => {
-    const addResp = await fetch(`http://${process.env.FETCH_ENDPOINT}/fetch`, {
-        method: 'GET',      
-    });
-
-    console.log("Fetching base64 image")
-
-    var result = await addResp.json()
-    res.send({result}) 
-});
-
-router.get('/moderate', upload.any(), async(req, res) => {
-    let approve = req.query.approve;
-    let id = req.query.id;
-    const moderateResp = await fetch(`http://${process.env.MODERATE_ENDPOINT}/moderate?approve=${approve}&id=${id}`, {
-        method: 'GET'
-    })
-
-    var result = await moderateResp.text()
-    res.send({result})
 })
