38aa88452fa49f4a9a64703487265eb77b9a343e
emsesc <emseschen@gmail.com>
2021-09-03 21:52:20 -0400

    moderate lists all unapproved

--- services/moderate-service/helpers.go
+++ services/moderate-service/helpers.go
@@ -3,12 +3,24 @@ package main
 import (
 	"os"
 	"log"
+	"fmt"
 	"database/sql"
 	_"github.com/go-sql-driver/mysql"
   )
 
 var password string = os.Getenv("PASSWORD")
 var endpoint string = os.Getenv("HOST")
+type Hats struct {
+	Key 		  int 	 `field:id`
+    ID            string `field:"keyId"`                      
+    Url           string `field:"url"`           
+    FileName      string `field:"fileName"`           
+    Description   string `field:"description"`
+    Approve       string `field:"approve"`
+}
+
+var hats []Hats
+var hat Hats
 
 func ApprovePicture(id string) string {
 	
@@ -61,6 +73,45 @@ func DeletePicture(id string) string {
 		result = "We're sorry that image was not suitable. We have deleted the image with an id of " + id + "."
 	}
 	defer insert.Close()
+
+	return result
+}
+
+func UnmoderatedPic() string {
+	var result string
+
+	// open connection
+	db, err := sql.Open("mysql", "admin:" + password + "@tcp(" + endpoint + ":3306)/main")
+	if err != nil {
+		log.Print("Database unreachable.")
+		result = "We had trouble connecting to the database."
+		// panic(err.Error())
+	  }
+	defer db.Close()
+	
+	// query for id and drop picture
+	rows, err := db.Query("SELECT * FROM main.images WHERE approve='false'")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		hat := new(Hats)
+
+		err = rows.Scan(&hat.Key, &hat.ID, &hat.Url, &hat.FileName, &hat.Description, &hat.Approve)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("hat: ", hat)
+		hats = append(hats, *hat)
+	}
+
+	result = fmt.Sprint(hats)
+
+	if err := rows.Err(); err != nil {
+		fmt.Println(err)
+	}
 
 	return result
 }
