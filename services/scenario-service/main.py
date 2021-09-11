5c87a30a239f0b74549726da29f352d1635f82d3
emsesc <emseschen@gmail.com>
2021-09-11 11:10:56 -0400

    small tweaks

--- services/scenario-service/main.py
+++ services/scenario-service/main.py
@@ -6,17 +6,17 @@ app = Flask(__name__)
 @app.route("/scenario1", methods=['GET'])
 def index() -> str:
     response = requests.post("http://locust-service-1:8089/swarm", {"locust_count":10, "hatch_rate":1})
-    print(response.content)
+    return response.content
 
 @app.route("/scenario2", methods=['GET'])
 def index() -> str:
     response = requests.post("http://locust-service-2:8089/swarm", {"locust_count":10, "hatch_rate":1})
-    print(response.content)
+    return response.content
 
 @app.route("/scenario3", methods=['GET'])
 def index() -> str:
     response = requests.post("http://locust-service-3:8089/swarm", {"locust_count":10, "hatch_rate":1})
-    print(response.content)
+    return response.content
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
