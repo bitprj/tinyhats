6916301777e9c6cb269b25060b17c0bd94e71e7e
emsesc <emseschen@gmail.com>
2021-09-18 13:13:34 -0400

    fix mysql version issue (changed mysql image)

--- services/upload-service/src/helpers.js
+++ services/upload-service/src/helpers.js
@@ -1,15 +1,8 @@
 import generateUniqueId from 'generate-unique-id'
-import mysql from 'mysql'
+import mysql from 'mysql2'
 
 const HOST = process.env.HOST;
 const PASSWORD = process.env.PASSWORD;
-
-const con = mysql.createConnection({
-    host: HOST,
-    port: '3306',
-    user: "admin",
-    password: PASSWORD,
-});
 
 export const fileExt = (ext) => {
     if (ext == "image/png") {
@@ -33,9 +26,17 @@ export const uniqueId = () => {
 }
 
 export async function push2RDS(key, ext, name, base64) {
+    const con = mysql.createConnection({
+        host: HOST,
+        port: '3306',
+        user: "admin",
+        password: PASSWORD,
+    });
+
     con.connect(function(err) {
-        con.query(`INSERT INTO main.images (keyId, fileName, base64, description, approve) VALUES ('${key}', '${key + ext}', '${base64}', '${name}', 'false')`, function(err, result, fields) {
-            if (err) console.log(err);
+        console.log(err)
+        con.query(`INSERT INTO main.images (keyId, base64, fileName, description, approve) VALUES ('${key}', '${base64}', '${key + ext}', '${name}', 'false')`, function(err, result, fields) {
+            // if (err) console.log(err);
             if (result) console.log({key: key, fileName: key + ext, base64: base64, description: name, approve: "false"});
             if (fields) console.log(fields);
         });
