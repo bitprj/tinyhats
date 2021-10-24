f932d2df05cf2b5dff340311e98f6b2b2c843c39
Daniel Kim <dgkim@ucdavis.edu>
2021-10-24 00:34:59 -0700

    added

--- services/scenario-service/main.py
+++ services/scenario-service/main.py
@@ -4,17 +4,17 @@ import requests
 app = Flask(__name__)
 
 @app.route("/scenario1", methods=['GET'])
-def index() -> str:
+def scenario1() -> str:
     response = requests.post("http://locust-service-1:8089/swarm", {"locust_count":10, "hatch_rate":1})
     return response.content
 
 @app.route("/scenario2", methods=['GET'])
-def index() -> str:
+def scenario2() -> str:
     response = requests.post("http://locust-service-2:8089/swarm", {"locust_count":10, "hatch_rate":1})
     return response.content
 
 @app.route("/scenario3", methods=['GET'])
-def index() -> str:
+def scenario3() -> str:
     response = requests.post("http://locust-service-3:8089/swarm", {"locust_count":10, "hatch_rate":1})
     return response.content
 
