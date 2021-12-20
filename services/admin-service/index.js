f465adde6ea00bce77c2ed1dd487dd14756af988
emsesc <emseschen@gmail.com>
2021-12-20 11:44:20 -0500

    hooked up mockup, admin, catalog

--- services/admin-service/index.js
+++ services/admin-service/index.js
@@ -15,10 +15,10 @@ app.listen(PORT, () => {
     console.log(`API Gateway started on port ${PORT}`)
 })
 
-router.post('/admin', upload.any(), async(req, res) => {
+router.post('/', upload.any(), async(req, res) => {
     var previews = []
     console.log(req.files)
-    let name = req.params["description"]
+    let name = req.body.name
     let hat = req.files[0].buffer
     const models = ["https://image.freepik.com/free-photo/close-up-shot-pretty-woman-with-perfect-teeth-dark-clean-skin-having-rest-indoors-smiling-happily-after-received-good-positive-news_273609-1248.jpg", "https://media.istockphoto.com/photos/handsome-young-man-on-white-background-picture-id523478288?k=20&m=523478288&s=612x612&w=0&h=Fg8yDwFhbB4XljB1aCclYbjJpUlRwB-jcPPFfd1Iteg="]
 
