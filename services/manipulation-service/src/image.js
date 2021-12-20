faf7aead027f6a8521453c1cf5620fb100468c40
emsesc <emseschen@gmail.com>
2021-12-19 19:11:42 -0500

    attempt 1

--- services/manipulation-service/src/image.js
+++ services/manipulation-service/src/image.js
@@ -6,7 +6,7 @@ require('@tensorflow/tfjs-node');
 const { Canvas, Image, ImageData } = canvas  
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
 
-const findBaby = async (baby) => {
+const findFace = async (baby) => {
   const image = await canvas.loadImage(baby)
   await faceapi.nets.ssdMobilenetv1.loadFromDisk('./weights')
   // const Canvas = canvas.createCanvas(image.width, image.height)
@@ -24,7 +24,7 @@ const findBaby = async (baby) => {
   // {"_x":225.59293228387833,"_y":122.78662695563085,"_width":183.89773482084274,"_height":181.8649869230835}
 }
 
-const overlayHat = async (hat, result, baby, translate, rotate) => {
+const overlayHat = async (hat, result, baby) => {
   let hatImg = await Jimp.read(hat);
   const image = await Jimp.read(baby);
   let jimpFace = image.bitmap
@@ -37,11 +37,8 @@ const overlayHat = async (hat, result, baby, translate, rotate) => {
   //  BoundingBox.Width:      ${data.BoundingBox.Width}`)
 
   hatImg = await hatImg.resize(width, height)
-  hatImg = await hatImg.rotate(rotate)
 
-  translate = translate * 0.3
-
-  image.composite(hatImg, left - width*translate, top - height*1.2, {
+  image.composite(hatImg, left - width, top - height*1.2, {
     mode: Jimp.BLEND_SOURCE_OVER,
     opacityDest: 1,
     opacitySource: 0.9
@@ -50,5 +47,27 @@ const overlayHat = async (hat, result, baby, translate, rotate) => {
   return await image.getBase64Async(Jimp.MIME_PNG)
 }
 
-exports.findBaby = findBaby
+const uploadPreview = async (description, hat, preview1, preview2) => {
+  const con = mysql.createConnection({
+    host: HOST,
+    port: '3306',
+    user: "admin",
+    password: PASSWORD,
+  });
+
+  con.connect(function(err) {
+      console.log(err)
+      con.query(`INSERT INTO main.images (description, hat, preview1, preview2) VALUES ('${description}', '${hat}', '${preview1}', '${preview2}')`, function(err, result, fields) {
+          if (err) console.log(err);
+          if (result) console.log({description: `${description}`});
+          if (fields) console.log(fields);
+      });
+      // connect to mysql and push data
+  });
+
+  return {description: `${description}`}
+}
+
+exports.findFace = findFace
 exports.overlayHat = overlayHat
+exports.uploadPreview = uploadPreview
